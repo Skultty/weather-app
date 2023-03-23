90922b60e8dca3d171b0c5e6e32de030977d8ad8
Skultty <samuel1skultety@gmail.com>
2023-03-23 10:26:54 +0100

    weather data is now displayed on the page

--- script.js
+++ script.js
@@ -12,10 +12,29 @@ document.getElementById("submit").addEventListener("click", function (event) {
   document.getElementById("modal").classList.add("hidden");
 });
 
+// add event listener for change location
+document
+  .getElementById("change-location")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("modal").classList.remove("hidden");
+  });
+
 async function getWeather(city) {
   let response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=7c6fdbf7f5714df984393840232003&q=${city}&aqi=no`
   );
   let data = await response.json();
   console.log(data);
+  displayWeather(data);
+}
+
+function displayWeather(data) {
+  document.getElementById("city-name").textContent = data.location.name;
+  console.log(document.getElementById("city-name"));
+  document.getElementById("current-temp").textContent = data.current.temp_c;
+  document.getElementById("weather-description").textContent =
+    data.current.condition.text;
+  document.getElementById("weather-icon").src =
+    "https:" + data.current.condition.icon;
 }
